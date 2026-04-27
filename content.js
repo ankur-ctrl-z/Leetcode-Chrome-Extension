25b69501dda7d13d13c29f22427f3504b6047fdd
ANKUR SHARMA <ankur143513@gmail.com>
2026-04-27 21:33:34 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => { 
   if (request.type === "GET_PROBLEM_DATA") {
-    const description = document.querySelector('div[data-track-load="description_content"]')?.innerText 
+    const description = document.querySelector('div[data-track-load="description_content"]')?.innerText  
                         ||   document.querySelector('.elfjS')?.innerText 
                         || "Description not found";
 
