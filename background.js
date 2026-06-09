a4a5a44cac9e7f65a2540494dcab464730f1c814
ANKUR SHARMA <ankur143513@gmail.com>
2026-06-09 20:03:19 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(() => { 
   chrome.storage.sync.get(["groqApiKey"], (data) => {
     if (!data.groqApiKey)  chrome.runtime.openOptionsPage(); 
   });
